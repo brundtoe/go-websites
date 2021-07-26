bb1af66299336b9c9470b0ea2c023f59abf961c0
Jackie <brundtoe@outlook.dk>
2021-07-26 16:42:35 +0200

    eksempel på http.NewRequest

---
+++ cmd/client/authors.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://victoria.test/api/authors/12", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
+}
