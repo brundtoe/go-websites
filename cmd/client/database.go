9d2fe4984c736d0c4b5cda4199cf356ca484c969
Jackie <brundtoe@outlook.dk>
2023-12-16 10:55:37 +0100

    note om opdatering af configfilen bookstore.env

--- cmd/client/database.go
+++ cmd/client/database.go
@@ -12,6 +12,7 @@ func main() {
 	var db *sql.DB
 
 	// Fungerer kun når MariaBD er installeret på hosten hvor programmet udføres
+	// Filen $HOME/.config/brundtoe/bookstore.env skal være opdateret med DB_HOST
 
 	db, err := dataobjects.GetConnection()
 	if err != nil {
